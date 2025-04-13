e34616fe9add14597dc4cf14f8365a4c34b61d82
venkata trinadh <venkatatrinadh4444@gmail.com>
2025-04-13 22:00:03 +0530

    updated the backend url

--- vendor/src/api's/Api.jsx
+++ vendor/src/api's/Api.jsx
@@ -1,4 +1,3 @@
-export const API_URI="http://localhost:8000"
+/* export const API_URI="http://localhost:8000" */
 
-/* export const API_URI="https://swiggy-clone-backend-qgso.onrender.com"
- */
+export const API_URI="https://multi-vendor-swiggy-clone.onrender.com"
